0d4ed1379e355ee8051b3479b8375720502901b8
Eli Goldberg <lgdragon28@gmail.com>
2015-07-02 00:28:22 +0300

    Added tests

--- src/taki/server/GameLogic.java
+++ src/taki/server/GameLogic.java
@@ -14,6 +14,7 @@ public class GameLogic {
 
 	final private int DEFAULT_NUM_DECKS = 2;
 	final private int DECK_SIZE = 56;
+	final private int INITIAL_HAND_SIZE = 8;
 
 	private ArrayList<GameCard> _deck;
 	private HashMap<String, ArrayList<GameCard>> _players;
@@ -24,7 +25,8 @@ public class GameLogic {
 	}
 
 	public void setPlayers(String[] players) {
-		_players.clear();
+		_players = new HashMap<String, ArrayList<GameCard>>();
+		
 		for (String player : players) {
 			_players.put(player, new ArrayList<>());
 		}
@@ -40,10 +42,27 @@ public class GameLogic {
 		shuffleCards();
 	}
 
+	public void startGame() {
+		handCardsToPlayers();
+	}
+	
+	private void handCardsToPlayers() {
+		if (_players != null) {
+			String[] playerNames = new String[_players.size()];
+			playerNames = _players.keySet().toArray(playerNames);
+			for (String player : playerNames) {
+				for (int i = 0; i < INITIAL_HAND_SIZE; i++) {
+					_players.get(player).add(_deck.get(0));
+					_deck.remove(0);
+				}
+			}
+		}
+	}
+	
 	private void shuffleCards() {
 		Collections.shuffle(_deck);
 	}
-
+	
 	private GameCard[] generateDeck(int nDecks) {
 		GameCard[] deck = new GameCard[DECK_SIZE * nDecks];
 		CardType[] cardTypes = CardType.values();
--- src/test/GameCardTest.java
+++ src/test/GameCardTest.java
@@ -1,6 +1,8 @@
 package test;
 
 import java.util.ArrayList;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -10,8 +12,30 @@ import taki.server.GameLogic;
 public class GameCardTest {
 
 	@Test
-	public void test() {
+	public void testCreateDeck() {
 		GameLogic logic = new GameLogic();
+		printDeck(logic);
+	}
+	
+	@Test
+	public void testHandCards() {
+		GameLogic logic = new GameLogic();
+		String[] players = new String[] {"Tal", "Moshe", "Itzik", "Shalom", "David", "Gal", "Chen",
+										 "Nir", "Assaf", "Matan", "Lior", "Maor", "Omri", "Itai"};
+		logic.setPlayers(players);
+		logic.startGame();
+		
+		for(String player : players) {
+			System.out.println(player);
+			for(GameCard card : logic.getPlayers().get(player)) {
+				System.out.println("\t" + card);
+			}
+		}	
+
+		printDeck(logic);
+	}
+	
+	private void printDeck(GameLogic logic) {
 		ArrayList<GameCard> deck = logic.getDeck();
 		
 		for(GameCard card : deck) {
